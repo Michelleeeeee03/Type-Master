6ebf7cb5f6dd6aab01c0c1bc018d64ef7fab376f
Michelleeeeee03 <michellereyes7345@gmail.com>
2026-02-25 23:12:39 -0400

    calculations

--- Type Master.py
+++ Type Master.py
@@ -6,3 +6,27 @@ words = ["Pneumonoultramicroscopicsilicovolcanoconiosis",
          "Sushi", "Nice", "Yeah", "The", "One",
          "Two", "Easy"]
 random.shuffle(words)
+
+correct = 0
+wrong = 0
+
+print("\nWelcome to the Typing Master Game!")
+print("\nType the given word correctly to score points and win.")
+for i in range(len(words)):
+    print("\n")
+    print(words[i])
+    answer = input("\nType the given word correctly: ")
+    if answer == words[i]:
+        print("\nCorrect")
+        correct += 1
+    else:
+        print ("\nIncorrect")
+        wrong += 1
+
+total_words = correct + wrong
+correct_words_percent = (correct/total_words) * 100
+wrong_words_percent = (wrong/total_words) * 100
+
+total_words = correct + wrong
+correct_words_percent = (correct/total_words) * 100
+wrong_words_percent = (wrong/total_words) * 100
