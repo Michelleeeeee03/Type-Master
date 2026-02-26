6488b0b95d962327bee5c2a5c186f2078b4eeb48
Michelleeeeee03 <michellereyes7345@gmail.com>
2026-02-25 23:01:53 -0400

    words

---
+++ Type Master.py
@@ -0,0 +1,8 @@
+import random
+words = ["Pneumonoultramicroscopicsilicovolcanoconiosis",
+         "Supercalifragilisticexpialidocious",
+         "Compassionate", "Enthusiastic", "Connoisseur",
+         "Questionnaire", "Hello", "Yellow",
+         "Sushi", "Nice", "Yeah", "The", "One",
+         "Two", "Easy"]
+random.shuffle(words)
